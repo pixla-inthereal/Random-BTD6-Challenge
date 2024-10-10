276192d0ce9bf598a9697f15e422301e507e9cb0
pixla <itsdapix5@gmail.com>
2024-10-10 14:26:09 -0400

    Add files via upload

---
+++ randomBTD6Challenge/randomBTD6Challenge.py
@@ -0,0 +1,189 @@
+from random import choice, randint
+
+
+def ran_hero():
+    """Randomizes Hero"""
+    heroes = [
+        "Quincy",
+        "Gwendolin",
+        "Striker Jones",
+        "Obyn Greenfoot",
+        "Rosalia",
+        "Captain Churchill",
+        "Benjamin",
+        "Pat Fusty",
+        "Ezili",
+        "Adora",
+        "Etienne",
+        "Sauda",
+        "Admiral Brickell",
+        "Psi",
+        "Geraldo",
+        "Corvus"
+    ]
+    return choice(heroes)
+
+
+def ran_difficulty():
+    '''Randomizes Difficulty and Mode'''
+    difficulties = [
+        "Easy",
+        "Medium",
+        "Hard"
+    ]
+    modes = [
+        "Standard",
+        "Apopalypse",
+        "Reverse",
+        "Half Cash",
+        "Double HP MOABs",
+        "Alternate Bloons Rounds",
+        "Impoppable",
+        "CHIMPS"
+    ]
+    return choice(difficulties) + " - " + choice(modes)
+
+
+def ran_map():
+    """Randomizes Map"""
+    maps = [
+        "Monkey Meadow",
+        "In the Loop",
+        "Middle of the Road",
+        "Tinkerton",
+        "Tree Stump",
+        "Town Center",
+        "One Two Tree",
+        "Scrapyard",
+        "The Cabin",
+        "Resort",
+        "Skates",
+        "Lotus Island",
+        "Candy Falls",
+        "Winter Park",
+        "Carved",
+        "Park Path",
+        "Alpine Run",
+        "Frozen Over",
+        "Cubism",
+        "Four Circles",
+        "Hedge",
+        "End of the Road",
+        "Logs",
+        "Luminous Cove",
+        "Sulfur Springs",
+        "Water Park",
+        "Polyphemus",
+        "Covered Garden",
+        "Quarry",
+        "Quiet Street",
+        "Bloonarius Prime",
+        "Balance",
+        "Encrypted",
+        "Bazaar",
+        "Adora's Temple",
+        "Spring Spring",
+        "Kartsndarts",
+        "Moon Landing",
+        "Haunted",
+        "Downstream",
+        "Firing Range",
+        "Cracked",
+        "Streambed",
+        "Chutes",
+        "Rake",
+        "Spice Islands",
+        "Ancient Portal",
+        "Castle Revenge",
+        "Dark Path",
+        "Erosion",
+        "Midnight Mansion",
+        "Sunken Columns",
+        "X Factor",
+        "Mesa",
+        "Geared",
+        "Spillway",
+        "Cargo",
+        "Pat's Pond",
+        "Peninsula",
+        "High Finance",
+        "Another Brick",
+        "Off the Coast",
+        "Cornfield",
+        "Underground",
+        "Glacial Trail",
+        "Dark Dungeons",
+        "Sanctuary",
+        "Ravine",
+        "Flooded Valley",
+        "Infernal",
+        "Bloody Puddles",
+        "Workshop",
+        "Quad",
+        "Dark Castle",
+        "Muddy Puddles",
+        "#OUCH"
+    ]
+    return choice(maps)
+
+
+def ran_towers():
+    """Randomizes Towers and Paths"""
+    towers = [
+        "Dart Monkey",
+        "Boomerang Monkey",
+        "Bomb Shooter",
+        "Tack Shooter",
+        "Ice Monkey",
+        "Glue Gunner",
+        "Sniper Monkey",
+        "Monkey Sub",
+        "Monkey Buccaneer",
+        "Monkey Ace",
+        "Heli Pilot",
+        "Mortar Monkey",
+        "Dartling Gunner",
+        "Wizard Monkey",
+        "Super Monkey",
+        "Ninja Monkey",
+        "Alchemist",
+        "Druid",
+        "Mermonkey",
+        "Banana Farm",
+        "Spike Factory",
+        "Monkey Village",
+        "Engineer Monkey",
+        "Beast Handler"
+    ]
+    tower_list = ""
+
+    ## chooses each tower and randomly determines paths
+    for i in towers:
+        tower_list += i + ": " + str(randint(0, 5)) + "-" + str(randint(0, 5)) + "-" + str(randint(0, 5)) + "\n"
+
+    return tower_list
+
+
+def ran_rounds():
+    """Randomly Decides Rounds"""
+    ## Always Round 1 Start, can go up to round 140
+    return "Rounds 1-" + str(randint(1, 140))
+
+
+def create_challenge():
+    """Gets the other random functions and compiles them together"""
+    challenge = (ran_map() + " || " + ran_difficulty() + "\n" +
+                 ran_rounds() + "\n\n" +
+                 ran_hero() + "\n" + ran_towers())
+    return challenge
+
+
+def main():
+    ## Creates a text file with the challenge
+    f = open("challenge.txt", "w")
+    f.write(create_challenge())
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
